a49d3a9b32bbc4d25ea855d30048c6573d8a60e3
ACER <mahanasty@mail.ru>
2020-11-06 13:51:21 +0300

    add material-ui

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,4 +1,45 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+// получаем данные контактов в отдельную функцию выносим логику
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
+        setIsLoading(false);
+        setIsError(false);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getContacts();
+  }, []);
+
+  return { data, isLoading, isError };
+};
+
+// компонента Контакты
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+
+  if (contacts.isLoading) {
+    return <div>LOADING</div>;
+  }
+  if (contacts.isError) {
+    return <div>...ERROR</div>;
+  }
+  return <div>Contacts {contacts.data[0].name.first}</div>;
 };
